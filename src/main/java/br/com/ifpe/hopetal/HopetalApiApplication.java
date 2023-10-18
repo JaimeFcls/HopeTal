1b0c263988c1501ad5b6de67600df5d8b8e007b1
JaimeFcls <119618310+JaimeFcls@users.noreply.github.com>
2023-10-18 14:20:48 -0300

    1 commit

---
+++ src/main/java/br/com/ifpe/hopetal/HopetalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.hopetal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopetalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopetalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/hopetal/HopetalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.hopetal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopetalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
